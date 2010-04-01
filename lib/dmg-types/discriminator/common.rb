c3be842624024258949cc31b1f7498fe34cf3cf0
Dawid Marcin Grzesiak <dmg@dmg.pl>
2010-04-01 14:12:27 +0200

    new architecture of Discriminator (class based)

---
+++ lib/dmg-types/discriminator/common.rb
@@ -0,0 +1,33 @@
+require 'dm-core'
+
+module DataMapper
+  module Types
+    class CommonDiscriminator < Discriminator
+      def self.dump(value, property)
+        raise 'implement'
+      end
+      
+      def self.load(value, property)
+        property.model.find_const(value)
+      end
+
+      def self.bind(property)
+        property.model.discriminator = Discriminator.new(property)
+      end
+      
+      class Discriminator < DataMapper::Model::Discriminator
+        def initialize(property)
+          super()
+          @property = property
+        end
+        
+        def discriminate(record, bag)
+          case klass = record[@property]
+            when Class then klass
+            else @property.type.load(klass.to_s, @property)
+          end
+        end
+      end
+    end
+  end # module Types
+end # module DataMapper
--- lib/dmg-types/discriminator/demodulized.rb
+++ lib/dmg-types/discriminator/demodulized.rb
@@ -1,8 +1,9 @@
 require 'dm-core'
+require "#{File.dirname(__FILE__)}/common.rb"
 
 module DataMapper
   module Types
-    class DemodulizedDiscriminator < Discriminator
+    class DemodulizedDiscriminator < CommonDiscriminator
       primitive String
       default   lambda { |resource, property| resource.model }
       required  true
@@ -10,19 +11,6 @@ module DataMapper
       def self.dump(value, property)
         ActiveSupport::Inflector.demodulize(value)
       end
-      
-      def self.load(value, property)
-        property.model.find_const(value)
-      end
-      
-      def self.bind(property)
-        property.model.discriminator = lambda do |record|
-          case klass = record[property]
-            when Class then klass
-            else load(klass.to_s, property)
-          end
-        end
-      end
-    end # class DemodulizedDiscriminator
+    end
   end # module Types
 end # module DataMapper
